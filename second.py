91c711420305e97271706533f8cecdd5ffe7bfc3
Pracser Tamas <t.pracser@gmail.com>
2016-12-05 09:40:16 +0100

    second

--- second.py
+++ second.py
@@ -4,3 +4,14 @@
 # If any problem raises with the file output, the function should not break, but return False.
 # Example: when called with the following two parameters: "tree.txt", "apple",
 # the function should write "appleappleapple" to the file "tree.txt", and return True.
+
+def write_to_file(file_name, string):
+    try:
+        fr = open(file_name, "w")
+        fr.write(string * 10)
+        fr.close
+        return True
+    except:
+        return False
+
+print(write_to_file("second_tree.txt", "apple"))
